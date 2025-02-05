678a1621292d99094722570e8801aaaeaa4a4a95
ManojKumar R <127740667+Manoj1904@users.noreply.github.com>
2025-02-05 09:52:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u manoj1904 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u manoj1904 -p Manoj@1904
+    docker tag test manoj1904/sample
+    docker push manoj1904/sample
     
