023ea1fff3058eec6befd4c6bde69765abfb6ecd
ManojKumar R <127740667+Manoj1904@users.noreply.github.com>
2025-02-07 10:18:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u manoj1904 -p Manoj@1904
-    docker tag test manoj1904/sample
-    docker push manoj1904/sample
+    docker tag test manoj1904/jenkins_task2
+    docker push manoj1904/jenkins_task2
     
